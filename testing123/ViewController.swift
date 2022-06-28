5a7298c76abb005e8e120284b0400a42799edf34
Shanaya Kabnur <skabnur66@gmail.com>
2022-06-28 14:47:42 -0500

    Initial Commit

---
+++ testing123/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  testing123
+//
+//  Created by Shanaya Kabnur on 6/28/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ testing123UITests/testing123UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  testing123UITestsLaunchTests.swift
+//  testing123UITests
+//
+//  Created by Shanaya Kabnur on 6/28/22.
+//
+
+import XCTest
+
+class testing123UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
